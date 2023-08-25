62d7ca7c2ec6bddce7f38c2370a534d3cfcf213b
daniel14gonc <gon20293@uvg.edu.gt>
2023-08-24 20:04:11 -0600

    add: colisiones inicial
    Co-authored-by: Aristondo01 <Aristondo01@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
 
 #define WIDTH 800
@@ -29,8 +30,10 @@ class Serpiente {
         len = l;
         x = new int[l];
         y = new int[l];
+        std::uniform_int_distribution<> distribution_pos(0, 1);
+        int latest = 0;
         for (int i = 0; i < l; i++) {
-            x[i] = initial_x + i * SQUARE_SIZE;
+            x[i] = initial_x;
             y[i] = initial_y;
         }
         
@@ -42,6 +45,9 @@ class Serpiente {
         color[1] = distribution_color(gen);
         color[2] = distribution_color(gen);
         color[3] = distribution_intensidad(gen);
+    }
+    void colision(){
+        len -=1;
     }
 
     void movimiento(int mov){
@@ -93,90 +99,154 @@ class Serpiente {
     
 };
 
+void spawn_snakes(Serpiente* serpientes[], int x, int l) {
+    for (int i = x; i < x + 5; i++) {
+        Serpiente* serpiente = new Serpiente(l);
+        serpientes[i] = serpiente;
+    }
+}
+
 
 int main(int argc, char* argv[]) {
     int n = 10;
     int l = 4;
+    bool valid = false;
     if (argc == 3) {
         n = strtol(argv[1], NULL, 10);
         l = strtol(argv[2], NULL, 10);
+
+        if (n > 0 && l > 0) {
+            valid = true;
+        }
+    }
+    int canvas[WIDTH][HEIGHT];
+    for (int i = 0; i < WIDTH; i++) {
+        for (int j = 0; j < HEIGHT; j++){
+            canvas[i][j] = -1;
+        }
     }
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("SDL Animation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    if (valid)
+    {
+        SDL_Init(SDL_INIT_VIDEO);
+        SDL_Window* window = SDL_CreateWindow("SDL Animation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    bool quit = false;
-    SDL_Event e;
+        bool quit = false;
+        SDL_Event e;
 
-    SDL_Rect squareRect = { 100, 100, 100, 100 };
-    int velocity = 1;
+        SDL_Rect squareRect = { 100, 100, 100, 100 };
+        int velocity = 1;
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distribution(1, 4);
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distribution(1, 4);
 
-    Serpiente* serpientes[n];
-    for (int i = 0; i < n; i++) {
-        Serpiente* serpiente = new Serpiente(l);
-        serpientes[i] = serpiente;
-    }
+        Serpiente* serpientes[n];
+        // for (int i = 0; i < n; i++) {
+        //     Serpiente* serpiente = new Serpiente(l);
+        //     serpientes[i] = serpiente;
+        // }
 
-    while (!quit) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
+        int cont = 0;
+        int current = 0;
+
+        while (!quit) {
+            while (SDL_PollEvent(&e)) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
             }
+
+            if (cont % 1000 == 0 && current < n) {
+                spawn_snakes(serpientes, current, l);
+                current += 5;
+            }
+
+            // Clear the renderer
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderClear(renderer);
+
+            for (int i = 0; i < current; i++) {
+                Serpiente* serpiente = serpientes[i];
+                int len_serpiente = serpiente->len;
+                for (int j = 0; j < len_serpiente; j++) {
+
+                    int valid_len = serpientes[i]->len;
+
+                    if (j >= valid_len) {
+                        continue;
+                    }
+
+                    int x = serpiente->x[j];
+                    int y = serpiente->y[j];      
+                    cout << canvas[y][x] << " " << i << endl;
+                    if (canvas[y][x] != i && canvas[y][x] != -1) {
+                        
+                        int x_eliminada = serpiente->x[valid_len-1];
+                        int y_eliminada = serpiente->y[valid_len-1];
+                        canvas[y_eliminada][x_eliminada] = -1;
+                        serpiente->colision();
+                        SDL_Rect rect_elim = {x_eliminada, y_eliminada, SQUARE_SIZE, SQUARE_SIZE};
+                        SDL_RenderFillRect(renderer, &rect_elim);
+                        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                        continue;
+                        
+                    }
+                    else
+                    {
+                        canvas[y][x] = i;
+                        int x_cola = serpiente->x[valid_len-1];
+                        int y_cola = serpiente->y[valid_len-1];
+                        canvas[y_cola][x_cola] = -1; 
+
+                    }
+
+                    SDL_Rect rect1 = {x, y, SQUARE_SIZE, SQUARE_SIZE};
+                    SDL_RenderFillRect(renderer, &rect1);
+                    // Draw the square
+                    int r = serpiente->color[0];
+                    int g = serpiente->color[1];
+                    int b = serpiente->color[2];
+                    int a = serpiente->color[3];
+                    if(j!=0 and j%2==0)
+                    {
+                        r = r+25;
+                        g = g-5;
+                        b = b-5;
+                    }
+                    else if (j == 0) {
+                        r = r+5;
+                        g = g-5;
+                        b = b-5;
+                        a = a/2;
+                    }
+                    SDL_SetRenderDrawColor(renderer, r, g, b, a);
+                }
+
+
+                serpiente->movimiento(distribution(gen));
+            }
+
+
+            // Update the screen
+            SDL_RenderPresent(renderer);
+
+            // Delay for smooth animation
+            SDL_Delay(40);
+
+            // Reset the drawing color to white
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         }
 
-        // Clear the renderer
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderClear(renderer);
-
-        for (int i = 0; i < n; i++) {
-            Serpiente* serpiente = serpientes[i];
-            for (int j = 0; j < l; j++) {
-                int x = serpiente->x[j];
-                int y = serpiente->y[j];
-                SDL_Rect rect1 = {x, y, SQUARE_SIZE, SQUARE_SIZE};
-                SDL_RenderFillRect(renderer, &rect1);
-             // Draw the square
-            int r = serpiente->color[0];
-            int g = serpiente->color[1];
-            int b = serpiente->color[2];
-            int a = serpiente->color[3];
-            if(j!=0 and j%2==0)
-            {
-                r = r+25;
-                g = g-5;
-                b = b-5;
-            }
-            else if (j == 0) {
-                r = r+5;
-                g = g-5;
-                b = b-5;
-                a = a/2;
-            }
-            SDL_SetRenderDrawColor(renderer, r, g, b, a);
-            }
-
-
-            serpiente->movimiento(distribution(gen));
-        }
-
-
-        // Update the screen
-        SDL_RenderPresent(renderer);
-
-        // Delay for smooth animation
-        SDL_Delay(40);
-
-        // Reset the drawing color to white
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
     }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    else
+    {
+        cout << "Revise que esta ingresando correctamente los parámetros" << endl;
+    }
     return 0;
 }
